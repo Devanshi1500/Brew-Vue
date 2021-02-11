50fedbf1a0e1babe916f3b8be46c6775b5761ae3
devanshi1500 <devanshi1500@gmail.com>
2021-02-11 12:12:38 +0530

    first vue lesson

---
+++ main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: "Yoga Mat",
+            description: "6 mm thick and strudy, hybrid jute fabric material",
+            price: 25,
+            brand: "Fitspree",
+            seller: "Homefit Mattresses",
+            instock: true,
+            rating: 4.6
+        }
+    }
+});
